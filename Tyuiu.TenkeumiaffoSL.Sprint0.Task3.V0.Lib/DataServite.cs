070870bb185e133a9aced8656b3b4fde85b37d17
Tyuiu.tenkeumiaffo@outlook.com <samueltenkeu@gmail.com>
2025-09-18 12:29:56 +0600

    Task3

---
+++ Tyuiu.TenkeumiaffoSL.Sprint0.Task3.V0.Lib/DataServite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TenkeumiaffoSL.Sprint0.Task3.V0.Lib
+{
+    public class DataServite
+    {
+        // реализация методa sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+       
+        
+        
+     
+      
+    }
+}
+
---
+++ Tyuiu.TenkeumiaffoSL.Sprint0.Task3.V0.Lib/IDataServite.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.TenkeumiaffoSL.Sprint0.Task3.V0.Lib
+{
+    public interface IDataServite
+    {
+        static abstract int Sum(int a, int b);
+    }
+}
